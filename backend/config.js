3c1226fd716fe13b52c3c2b600fa9d93fea8a46e
Omin Hemapriya <it21801372@my.sliit.lk>
2024-03-16 17:05:06 +0530

    Successfully connected to MongoDB

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const MONGO_URI = "mongodb+srv://root:123@online-shopping-mall-sy.bmysewi.mongodb.net/products?retryWrites=true&w=majority&appName=online-shopping-mall-system";
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, MONGO_URI } from "./config.js";
+import mongoose from "mongoose";
 
 const app = new express();
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-    });
+
+
+mongoose.connect(MONGO_URI) 
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err)
+        });
