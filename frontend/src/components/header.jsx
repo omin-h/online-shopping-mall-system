ce54b8bc2b1200fb529808455c90d00cf03e107b
Omin Hemapriya <it21801372@my.sliit.lk>
2024-03-16 21:36:20 +0530

    Create a home page and a header component for testing purposes

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,12 @@
+//for testing purposes
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Online Shopping Mall System</h1>
+        </header>
+    );
+};
+
+export default Header;
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import Header from "../components/header";
+
+const Home = () => {
+
+    return (
+        <div>
+            <Header />
+            <h2>Welcome to the Online Shopping Mall System</h2>
+        </div>
+    );
+
+};
+
+export default Home;
