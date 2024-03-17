86d083fd4dbafaa9041205fb68953e775c277ca6
Omin Hemapriya <it21801372@my.sliit.lk>
2024-03-17 12:23:29 +0530

    Budget collecting page created

---
+++ frontend/src/pages/budget.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BudgetForm from "../components/budgetForm";
+
+const Budget = () => {
+    return (
+        <div>
+            <BudgetForm />
+        </div>
+    );
+};  
+
+export default Budget;
