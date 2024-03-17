43e2a58d4273bec3256d9facd3cc0082ba34891d
Omin Hemapriya <it21801372@my.sliit.lk>
2024-03-17 22:45:38 +0530

    Footer created

--- frontend/src/pages/budget.jsx
+++ frontend/src/pages/budget.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/header";
 import BudgetForm from "../components/budgetForm";
+import Footer from "../components/footer";
 
 
 const Budget = () => {
@@ -8,6 +9,7 @@ const Budget = () => {
         <div>    
             <Header />     
             <BudgetForm />
+            <Footer />
         </div>
     );
 };  
