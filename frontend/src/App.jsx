3f211e46db81711248222f40ac9d8cc511d3af12
dunalpasindu <it21389542@my.sliit.lk>
2024-03-18 09:16:43 +0530

    customer_support function files added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import "./App.css";
-import { Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
+import CustomerSupport from "./pages/customer_support";
+
 
 const App = () => {
   return (
 
+
+
     <Routes> 
+
+
       <Route path="/" element={<Home />} />
+
+      <Route path="/customer_support" element={<CustomerSupport />} />
+      
+
+
+
+
+
     </Routes>
+
+  
 
   )
 }
