9af4c77b0168058e2e56b888c3cdcbdb3fee75aa
Omin Hemapriya <it21801372@my.sliit.lk>
2024-03-19 09:38:18 +0530

    New page for AI function created

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -3,11 +3,14 @@ import './header.css';
 import SearchBar from './searchBar';
 import logo from '../assets/logo.png';
 import user from '../assets/user.png';
+import { Link } from 'react-router-dom'; 
 
 const Header = () => {
     return (
         <div className='headerBar'>
-          <img src={logo} alt="logo" className='logo'/>
+          <Link to="/">
+            <img src={logo} alt="logo" className='logo'/>
+          </Link>
           <ul className='nav'>
             <li className='nav-li'><a href="/" className='linkText'>Home</a></li>
             <li className='nav-li'><a href="/upcoming" className='linkText'>Upcoming Items</a></li>
--- frontend/src/components/shoppingListComp.jsx
+++ frontend/src/components/shoppingListComp.jsx
@@ -1,15 +1,70 @@
 import React from "react";
+import Tshirt from '../assets/polo-shirt.webp';
+import Trouser from '../assets/trouser.jpg';
+import Bottle from '../assets/bottle.jpg';
+import Bag from '../assets/bag.jpg';
+import Cart from '../assets/cart.png';
+import './shoppingListComp.css';
 
 const ShoppingListComp = () => {
     return (
         <div>
-            <div className="list-bud">
-                <p className="list-bud-title">
-                    BUDGET: Lkr 20000 - Lkr 25000
-                </p>
+            
+            <div className="list-fill">
+                <div className="list-item1">
+                    <div className="list-item1-con">
+                        <img src={Tshirt} alt="onion" className="list-item1-img" />
+                    </div>
+                    <div className="list-item1-detail">
+                        <p className="list-item1-title">
+                            Emerald Polo T-Shirt
+                    
+                        </p>
+                        <p className="list-item1-price">
+                            Lkr 5000
+                        </p>
+                    </div>
+
+
+                </div>
+
+                <div className="list-item2">
+                    <div className="list-item2-con">
+                        <img src={Trouser} alt="onion" className="list-item2-img" />
+
+                    </div>
+
+
+                </div>
+
+                <div className="list-item3">
+                    <div className="list-item3-con">
+                        <img src={Bottle} alt="onion" className="list-item3-img" />
+
+                    </div>
+
+
+                </div>
+
+                <div className="list-item4">
+                    <div className="list-item4-con">
+                        <img src={Bag} alt="onion" className="list-item4-img" />
+
+                    </div>
+
+
+                </div>
+            </div>
+            <div className="list-end">
+                <div className="list-bud">
+                    <p className="list-bud-title">
+                        Total cost: Lkr 23000
+                    </p>
+                </div>
+
+                <div className="list-button-fill"><button type="submit" className="list-button">Add to Cart<img src={Cart} alt="Button Image" className="cart-logo"></img></button></div>
             </div>
 
-            
         </div>
     );
 }
