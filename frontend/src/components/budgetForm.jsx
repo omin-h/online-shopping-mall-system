5371942f97676f65103ead58347787b9317f85cf
Omin Hemapriya <it21801372@my.sliit.lk>
2024-03-21 18:26:55 +0530

    Update AI code

--- frontend/src/components/budgetForm.jsx
+++ frontend/src/components/budgetForm.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./budgetForm.css";
 import ai from '../assets/AI Logo.png';
+import { useNavigate } from "react-router-dom";
+
+
 
 const BudgetForm = () => {
+
+    const navigate = useNavigate(); // Initialize useNavigate
+
+    const handleClick = () => {
+        navigate("/shoppingList"); // Navigate to "/shoppingList" when button is clicked
+    };
+
     return (
         <div className="budget-form-fill"> <div className="budget-title"><p>Find best shopping list for your budget with AI</p></div>
 
@@ -23,7 +33,7 @@ const BudgetForm = () => {
             </form>
         </div>
 
-        <div className="bud-button-fill"><button type="submit" className="bud-button">Generate<img src={ai} alt="Button Image" className="ai-logo"></img></button></div>
+        <div className="bud-button-fill"><button type="button" onClick={handleClick} className="bud-button">Generate<img src={ai} alt="Button Image" className="ai-logo"></img></button></div>
 
         </div>
 
