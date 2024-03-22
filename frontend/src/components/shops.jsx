88da12b6b377fe745de6c34a0ac25132b32f885c
Omin Hemapriya <it21801372@my.sliit.lk>
2024-03-22 21:35:45 +0530

    Home page updated

---
+++ frontend/src/components/shops.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Adi from '../assets/adi.jpeg';
+import Aba from '../assets/aba.jpeg';
+import Bab from '../assets/bab.jpeg';
+import Calv from '../assets/calv.jpeg';
+import Jew from '../assets/jew.jpeg';
+import Dsi from '../assets/dsi.jpeg';
+import Die from '../assets/die.jpeg';
+import Fal from '../assets/fal.jpeg';
+import Nol from '../assets/nol.jpeg';
+import Nik from '../assets/nik.jpeg';
+import Lea from '../assets/lea.jpeg';
+import Bri from '../assets/bri.jpeg';
+import Cey from '../assets/cey.jpeg';
+import Cha from '../assets/cha.jpeg';
+import Coc from '../assets/coc.jpeg';
+import Cro from '../assets/cro.jpeg';
+import Dan from '../assets/dan.jpeg';
+import Ekk from '../assets/ekk.jpeg';
+import Fit from '../assets/fit.jpeg';
+import Fur from '../assets/fur.jpeg';
+import Ham from '../assets/ham.jpeg';
+import Bod from '../assets/bod.jpeg';
+import Dri from '../assets/dri.jpeg';
+import './shops.css';
+
+const Shops = () => {
+    return (
+        <div className="shops-fill">
+                    <div className="shop1"><img src={Adi} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Aba} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Bab} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Calv} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Jew} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Dsi} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Die} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Fal} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Nol} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Nik} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Lea} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Bri} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Cey} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Cha} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Coc} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Cro} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Dan} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Ekk} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Fit} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Fur} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Ham} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Bod} className="shop-logo1" alt="shop-logo"></img></div>
+                    <div className="shop1"><img src={Dri} className="shop-logo1" alt="shop-logo"></img></div>
+
+            </div>
+
+    );
+}
+
+export default Shops;
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -3,6 +3,7 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import Banner from '../assets/banner.jpg';
 import aiIcon from '../assets/AI Logo.png';
+import Shops from '../components/shops';
 import './home.css';
 
 const Home = () => {
@@ -14,11 +15,10 @@ const Home = () => {
             <div className="AI-banner">
                 <p className="AI-banner-text">Make your shopping list with AI</p>
                 <img src= {aiIcon} alt="AI Icon" className="AI-banner-icon"/>
-                <a href="/budget" class="AI-but">Try now</a>
-
-
-
+                <a href="/budget" class="AI-but">Try now</a>           
             </div>
+
+            <Shops />         
 
             <Footer />
         </div>
