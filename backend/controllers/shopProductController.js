c74509191491813584dfd33ae4621285f29b8ad0
Omin Hemapriya <it21801372@my.sliit.lk>
2024-04-09 15:05:31 +0530

    POST and GET methods succssefully working

--- backend/controllers/shopProductController.js
+++ backend/controllers/shopProductController.js
@@ -15,14 +15,41 @@ const createShopProduct = async (req, res) => {
         });
 
         await shopProduct.save();
-        res.status(201).json({ message: 'Product Created' });
+        res.status(201).json({ message: 'Product is added' });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Server Error' });
     }
 };
 
+// Function to get all shop products
+const getAllShopProducts = async (req, res) => {
+    try {
+        const products = await ShopProduct.find();
+        res.status(200).json(products);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
+
+// Function to get a specific shop product by productId
+const getShopProductById = async (req, res) => {
+    try {
+        const productId = req.params.productId;
+        const product = await ShopProduct.findOne({ productId: productId });
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
 
 export default {
-    createShopProduct
+    createShopProduct,
+    getAllShopProducts,
+    getShopProductById
 };
