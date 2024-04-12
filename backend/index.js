afa59c8b512d7083eb19f7fcfa383605761e1671
dunalpasindu <it21389542@my.sliit.lk>
2024-04-12 08:58:54 +0530

    fixed conflicts from pull

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
 import express from "express";
+import cors from "cors";
 import { PORT, MONGO_URI } from "./config.js";
 import mongoose from "mongoose";
 
+import ticketrouter from "./routes/ticketRoutes.js";
+
 
 const app = new express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/ticket", ticketrouter);
 
 mongoose.connect(MONGO_URI) 
     .then(() => {
