9e280027cacb271adaee648e4a7dd60d5fca6335
sachiraM <it21258930@my.sliit.lk>
2024-05-03 14:55:30 +0530

    Route path added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,9 @@ import Budget from "./pages/budget";
 import ShoppingList from "./pages/shoppingList";
 import Generate from "./pages/generate";
 
+import PrototypePage from "./pages/prototypepage";
+import BrandListPage from "./pages/brandlistpage";
+
 
 const App = () => {
   return (
@@ -39,6 +42,11 @@ const App = () => {
       <Route path="/budget" element={<Budget />} />
       <Route path="/generate" element={<Generate />} />
       <Route path="/shoppingList" element={<ShoppingList />} />
+
+
+      {/* sachira's routes */}
+      <Route path="/prototype" element={ <PrototypePage/>} />
+      <Route path="/brandlist" element={ <BrandListPage/>} />
   
       
     </Routes>
