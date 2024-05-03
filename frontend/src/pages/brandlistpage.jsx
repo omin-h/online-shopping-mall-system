f1d9ffaf75fc8e138d0dcf1efb3700cf7dc5fa1d
sachiraM <it21258930@my.sliit.lk>
2024-05-03 17:27:25 +0530

    Brandlist page created

---
+++ frontend/src/pages/brandlistpage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import BrandListmain from '../components/brandList';
+import Header from '../components/header';
+import Footer from '../components/footer';
+
+function BrandListPage() {
+  return (
+    <div>
+      <Header/>
+      <BrandListmain />
+    <Footer/>
+    </div>
+  );
+};
+
+export default BrandListPage;
