4939b5d161e6b17869750fc98b9f69bcc5ce0ad3
TALperera <it21262722@my.sliit.lk>
2024-05-04 01:08:54 +0530

    carete

--- frontend/src/components/shops.jsx
+++ frontend/src/components/shops.jsx
@@ -6,7 +6,7 @@ import Calv from '../assets/calv.jpeg';
 import Jew from '../assets/jew.jpeg';
 import Dsi from '../assets/dsi.jpeg';
 import Die from '../assets/die.jpeg';
-import Fal from '../assets/fal.jpeg';
+import Cro from '../assets/cro.jpeg';
 import Nol from '../assets/nol.jpeg';
 import Nik from '../assets/nik.jpeg';
 import Lea from '../assets/lea.jpeg';
@@ -14,7 +14,7 @@ import Bri from '../assets/bri.jpeg';
 import Cey from '../assets/cey.jpeg';
 import Cha from '../assets/cha.jpeg';
 import Coc from '../assets/coc.jpeg';
-import Cro from '../assets/cro.jpeg';
+import Fal from '../assets/fal.jpeg';
 import Dan from '../assets/dan.jpeg';
 import Ekk from '../assets/ekk.jpeg';
 import Fit from '../assets/fit.jpeg';
@@ -28,28 +28,28 @@ const Shops = () => {
     return (
         <div id="shops-section" className="shops-fill">
                     <a className="shop1" href="/shop/adi"><img src={Adi} className="shop-logo1" alt="shop-logo"></img></a>
-                    <div className="shop1"><img src={Aba} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Bab} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Calv} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Jew} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Dsi} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Die} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Fal} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Nol} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Nik} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Lea} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Bri} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Cey} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Cha} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Coc} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Cro} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Dan} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Ekk} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Fit} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Fur} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Ham} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Bod} className="shop-logo1" alt="shop-logo"></img></div>
-                    <div className="shop1"><img src={Dri} className="shop-logo1" alt="shop-logo"></img></div>
+                    <a className="shop1" href="/shop/aba"><img src={Aba} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/bab"><img src={Bab} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/calv"><img src={Calv} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/jew"><img src={Jew} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/dsi"><img src={Dsi} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/die"><img src={Die} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/cro"><img src={Cro} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/nol"><img src={Nol} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/nik"><img src={Nik} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/lea"><img src={Lea} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/bri"><img src={Bri} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/cey"><img src={Cey} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/cha"><img src={Cha} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/coc"><img src={Coc} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/fal"><img src={Fal} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/dan"><img src={Dan} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/ekk"><img src={Ekk} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/fit"><img src={Fit} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/fur"><img src={Fur} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/ham"><img src={Ham} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/bod"><img src={Bod} className="shop-logo1" alt="shop-logo"></img></a>
+                    <a className="shop1" href="/shop/dri"><img src={Dri} className="shop-logo1" alt="shop-logo"></img></a>
 
             </div>
 
--- frontend/src/pages/shop.jsx
+++ frontend/src/pages/shop.jsx
@@ -16,13 +16,101 @@ const Shop = () => {
             items: [
                 {
                     id: 'adi1',
+                    name: 'Shoes',
+                    description: 'Color: White',
+                    price: 9500.00,
+                    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIc3gulqFqpdQA7hitfIQ6Y837KgORkOdAPLG-5gykkg&s'
+                },
+                {
+                    id: 'adi2',
                     name: 'Essential Backpack',
                     description: 'Color: Black',
                     price: 9500.00,
-                    image: 'https://incarnage.com/cdn/shop/files/DSC00084.jpg?crop=center&height=300&v=1709712558&width=300'
+                    image: 'https://assets.adidas.com/images/w_1880,f_auto,q_auto/b7beee7c32d4438aaba3acb6001c2e7b_9366/FY7757_01_standard.jpg'
+                },
+                
+            ]
+        },
+
+        {
+            id: 'aba',
+            name: 'Abance',
+            items: [
+                {
+                    id: 'aba1',
+                    name: 'Dell laptop',
+                    description: 'Color: Black',
+                    price: 180000.00,
+                    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdV1bHDrMniRNhgkhK5Xu3HCoRmgmlFLyiEGmh-S0WxQ&s'
+                },
+                {
+                    id: 'aba2',
+                    name: 'VIVO Y20',
+                    description: 'Color: Blue',
+                    price: 9500.00,
+                    image: 'https://cdn.buyabans.com/media/catalog/product/cache/b26914b4315099761016f66ddcabadb6/n/e/new_proahjsu__sct.png'
                 },
             ]
         },
+        {
+            id: 'bab',
+            name: 'Baby Care',
+            items: [
+                {
+                    id: 'bab1',
+                    name: 'go cart stroller',
+                    description: 'Color: Black',
+                    price: 50000.00,
+                    image: 'https://babycare.lk/wp-content/uploads/2021/06/babycare-ace-stroller-2.png'
+                },
+                {
+                    id: 'bab2',
+                    name: 'baby care product',
+                    description: 'Color: blue and pink',
+                    price: 9500.00,
+                    image: 'https://softsensbaby.com/cdn/shop/files/Website_Banner_Kit.jpg?v=1669009361'
+                },
+            ]
+        },{
+            id: 'calv',
+            name: 'Calvin Klein',
+            items: [
+                {
+                    id: 'calv1',
+                    name: 'clavin klein t-shirt',
+                    description: 'Color: brown and Blue',
+                    price: 12000.00,
+                    image: 'https://i.ebayimg.com/images/g/sNkAAOSw6nRibVdE/s-l1200.webp'
+                },
+                {
+                    id: 'calv2',
+                    name: 'clavin klein perfume',
+                    description: 'Color: white ',
+                    price: 19500.00,
+                    image: 'https://objectstorage.ap-mumbai-1.oraclecloud.com/n/softlogicbicloud/b/cdn/o/products/80062764--1--1645173038.jpeg'
+                },
+            ]
+        },{
+            id: 'jew',
+            name: 'colombo jewellers',
+            items: [
+                {
+                    id: 'jew1',
+                    name: 'blue sapphire necklace',
+                    description: 'Color: whitegold and gemstone',
+                    price: 100000.00,
+                    image: 'https://media.timeout.com/images/103721525/750/562/image.jpg'
+                },
+                {
+                    id: 'jew2',
+                    name: 'gold necklace',
+                    description: 'Color: Gold',
+                    price: 150000.00,
+                    image: 'https://media.timeout.com/images/101903591/750/422/image.jpg'
+                },
+            ]
+        },
+
     ];
 
     const shop = shops.find(shop => shop.id === name);
